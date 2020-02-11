6a288902489b74301e44bd560a08ddf8fae12cfb
Stepan Karpenko <stepan.karpenko@gmail.com>
2020-02-10 22:35:01 -0800

    Environment setup script.
    Signed-off-by: Stepan Karpenko <stepan.karpenko@gmail.com>

---
+++ setenv.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# The VxEngine Project.
+# Environment configuration.
+
+echo ""
+echo "Setting environment for VxEngine."
+echo "================================="
+echo ""
+
+# Set work directory
+export VXENGINE_HOME=`pwd`
+echo "Workspace: $VXENGINE_HOME"
+
+echo ""
