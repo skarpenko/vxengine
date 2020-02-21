acec193fcdb43494c59f85c288d994a5cda01d68
Stepan Karpenko <stepan.karpenko@gmail.com>
2020-02-20 20:29:19 -0800

    hw/flp: Verilator-based multiplication testbench
    Signed-off-by: Stepan Karpenko <stepan.karpenko@gmail.com>

---
+++ hw/flp/vl/tb/vl_flp_mul_test.cxx
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2020 The VxEngine Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Floating point multiplication logic test (C++ main)
+ */
+
+#include <cstdint>
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <verilated.h>		// Defines common routines
+#if VM_TRACE
+# include <verilated_vcd_c.h>	// Trace file format header
+#endif
+#include "Vvl_flp_mul_test.h"	// From Verilating "vl_flp_mul_test.v"
+#include "vl_common.hxx"	// Common Verilator types
+
+// Floating point reference model
+#include "hwfmac.hxx"
+#include "common.hxx"
+
+
+#define SRAND_SEED	time(NULL)
+#define NITER		10000000000
+#define NPRIN		100000
+
+
+vluint64_t main_time = 0;	// Current simulation time
+
+
+// Called by $time in Verilog
+double sc_time_stamp()
+{
+	return main_time;
+}
+
+
+// Floating point multiplication reference model
+uint32_t mul_test(uint32_t a, uint32_t b)
+{
+	uint32_t r;
+
+	hwfmac::mul<uint32_t, uint64_t, 8, 23, 2>(a, b, r);
+
+	return r;
+}
+
+
+// MAIN
+int main(int argc, char **argv)
+{
+	std::cout << "FP multiplier logic test" << std::endl;
+	std::cout << "NITER = " << NITER << std::endl;
+
+	Verilated::commandArgs(argc, argv);	// Pass args to Verilator
+
+	// Create top-level instance
+	model_top<Vvl_flp_mul_test> top;
+
+#if VM_TRACE
+	VerilatedVcdC* tfp = 0;
+	Verilated::traceEverOn(true);	// Enable traces
+	tfp = new VerilatedVcdC;
+	top->trace (tfp, 99);		// Trace 99 levels of hierarchy
+	tfp->open("vlt_dump.vcd");	// Open the dump file
+#endif
+
+	aux::float_t a;
+	aux::float_t b;
+	aux::float_t rm, rh;
+
+	srand(SRAND_SEED);
+
+	// Main simulation loop
+	for (long long i = 0; i < NITER && !Verilated::gotFinish(); ++i) {
+		int r1 = rand();
+		int r2 = rand();
+		int r3 = rand();
+		int r4 = rand();
+		int s1 = rand() & 1;
+		int s2 = rand() & 1;
+		// Randomized inputs
+		a.f = (float) r1 / (float) r2;
+		b.f = (float) r3 / (float) r4;
+		a.s.sign = s1;
+		b.s.sign = s2;
+
+		rm.v = mul_test(a.v, b.v);	// Reference model result
+
+		top->i_a = a.v;
+		top->i_b = b.v;
+		top->eval();			// Evaluate model
+		rh.v = top->o_p;		// RTL model result
+
+#if VM_TRACE
+		if(tfp) tfp->dump(main_time);	// Dump waveforms
+#endif
+
+		// Check result
+		if(rm.v != rh.v) {
+			std::ios state(nullptr);
+			state.copyfmt(std::cout);
+			std::cout << "[!] "
+				<< std::setfill('0') << std::hex
+				<< std::setw(8) << rm.v << " != "
+				<< std::setw(8) << rh.v
+				<< "\t"
+				<< rm.f << " != " << rh.f
+				<< std::endl;
+			std::cout.copyfmt(state);
+		}
+
+		// Print progress
+		if(!(i % NPRIN))
+			std::cout << i << " / " << NITER << "\r";
+
+		++main_time;			// Time passes...
+	}
+
+	std::cout << std::endl << "Done." << std::endl;
+
+	top->final();	// Done simulating
+
+#if VM_TRACE
+	if(tfp) tfp->close();
+#endif
+
+	return 0;
+}
---
+++ hw/flp/vl/tb/vl_flp_mul_test.v
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2020 The VxEngine Project. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ * Floating point multiplication logic test
+ */
+
+module vl_flp_mul_test(
+	i_a,
+	i_b,
+	o_p
+);
+/* Inputs */
+input wire [31:0]	i_a;
+input wire [31:0]	i_b;
+/* Outputs */
+output wire [31:0]	o_p;
+
+
+/* FP32 multiplication test instance */
+flp_mul_test #(
+	.EWIDTH(8),
+	.SWIDTH(23),
+	.RSWIDTH(2)
+) fp32_mul_test (
+	.i_a(i_a),
+	.i_b(i_b),
+	.o_p(o_p)
+);
+
+
+endmodule /* vl_flp_mul_test */
