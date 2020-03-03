373bd15e5f8e8cf11231004a36db052d1f7deaa2
Stepan Karpenko <stepan.karpenko@gmail.com>
2020-03-02 21:58:24 -0800

    setenv: added check for SYSTEMC_HOME variable.
    Signed-off-by: Stepan Karpenko <stepan.karpenko@gmail.com>

--- setenv.sh
+++ setenv.sh
@@ -11,4 +11,11 @@ echo ""
 export VXENGINE_HOME=`pwd`
 echo "Workspace: $VXENGINE_HOME"
 
+# Check that SYSTEMC_HOME variable is set.
+if [ -z ${SYSTEMC_HOME+x} ]; then
+   echo "SystemC: Warning! SYSTEMC_HOME variable is not unset.";
+else
+   echo "SystemC: SYSTEMC_HOME = $SYSTEMC_HOME";
+fi
+
 echo ""
