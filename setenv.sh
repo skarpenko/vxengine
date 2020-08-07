0e064f96ca75d44bf664416d2bf5529c621387ea
Stepan Karpenko <stepan.karpenko@gmail.com>
2020-08-06 20:20:40 -0700

    env: added setting of VERILATOR_HOME environment variable.
    Signed-off-by: Stepan Karpenko <stepan.karpenko@gmail.com>

--- setenv.sh
+++ setenv.sh
@@ -8,14 +8,28 @@ echo "================================="
 echo ""
 
 # Set work directory
-export VXENGINE_HOME=`pwd`
-echo "Workspace: $VXENGINE_HOME"
+export VXENGINE_HOME=${PWD}
+echo "Workspace: ${VXENGINE_HOME}"
 
 # Check that SYSTEMC_HOME variable is set.
-if [ -z ${SYSTEMC_HOME+x} ]; then
-   echo "SystemC: Warning! SYSTEMC_HOME variable is not unset.";
+if [ -z ${SYSTEMC_HOME} ]; then
+   echo "SystemC: Warning! SYSTEMC_HOME variable is not set.";
 else
-   echo "SystemC: SYSTEMC_HOME = $SYSTEMC_HOME";
+   echo "SystemC: SYSTEMC_HOME = ${SYSTEMC_HOME}";
+fi
+
+# Check that VERILATOR_HOME variable is set and try to set it automatically if
+# it is not.
+if [ -z ${VERILATOR_HOME} ]; then
+   VERILATOR=$(which verilator)
+   export VERILATOR_HOME=${VERILATOR%/*/*}
+fi
+
+# Print VERILATOR_HOME if it was set successfully
+if [ -z ${VERILATOR_HOME} ]; then
+   echo "Verilator: Verilator not found! VERILATOR_HOME variable is not set.";
+else
+   echo "Verilator: VERILATOR_HOME = ${VERILATOR_HOME}";
 fi
 
 echo ""
