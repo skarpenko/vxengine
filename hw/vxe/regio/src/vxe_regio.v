172cbd684b450d094cc05a9b35b8d1d3981b9f8f
Stepan Karpenko <stepan.karpenko@gmail.com>
2022-08-17 21:07:00 -0700

    hw/vxe: added missing comment
    Signed-off-by: Stepan Karpenko <stepan.karpenko@gmail.com>

--- hw/vxe/regio/src/vxe_regio.v
+++ hw/vxe/regio/src/vxe_regio.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 The VxEngine Project. All rights reserved.
+ * Copyright (c) 2020-2022 The VxEngine Project. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -85,6 +85,7 @@ input wire [3:0]	i_intu_act;
 output wire [3:0]	o_intu_msk;
 output reg		o_intu_ack_vld;
 output reg [3:0]	o_intu_ack;
+/* Memory hub interface signals */
 output wire		o_cu_mas_sel;
 
 
