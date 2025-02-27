4b1e45bc63d830d2ba462d40ad42190925ee9f66
Stepan Karpenko <stepan.karpenko@gmail.com>
2025-02-26 22:32:20 -0800

    hw/vxe/vpu: fix noisy debug message
    Signed-off-by: Stepan Karpenko <stepan.karpenko@gmail.com>

--- hw/vxe/vec_unit/src/vxe_vpu_regu_ecu.v
+++ hw/vxe/vec_unit/src/vxe_vpu_regu_ecu.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2023 The VxEngine Project. All rights reserved.
+ * Copyright (c) 2020-2025 The VxEngine Project. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -91,7 +91,8 @@ begin
 	end
 	default: begin
 		o_ridx = VPU_REG_IDX_IGN;
-		$display("Wrong register index!\n");
+		if(i_disp)
+			$display("Wrong register index!\n");
 	end
 	endcase
 end
